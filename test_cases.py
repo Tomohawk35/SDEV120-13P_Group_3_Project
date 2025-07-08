dc3662e23dc674308a4a9889c80f473e9ce920f3
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2025-07-08 14:20:40 -0400

    Update test_cases.py
    Added additional commenting to the test_cases.py file

--- test_cases.py
+++ test_cases.py
@@ -1,10 +1,16 @@
+## Test Case file
+## Can be executed by running "pytest test_cases.py" in the command line
+
 import pytest
 from net_pay_calculator import main, input_employee_data, get_pay_rate, calculate_gross_pay, calculate_taxes, record_results
 
 
+# Assigned to Fatimatou Ibrahim
 def test_input_employee_data() -> None:
     pass
 
+
+# Assigned to Tyler Howard
 
 def test_get_pay_rate():
     assert get_pay_rate(1) == 20.00
@@ -12,13 +18,16 @@ def test_get_pay_rate():
     assert get_pay_rate(4) == 17.77
 
 
+# Assigned to DeMishia jackson
 def test_calculate_gross_pay() -> None:
     pass
 
 
+# Assigned to Willie Jones
 def test_calculate_taxes() -> None:
     pass
 
 
+# Assigned to Kevin White
 def test_record_results() -> None:
     pass
