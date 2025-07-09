f058329ff7fab79242db030b31cb25aceaf3c47a
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2025-07-09 10:48:55 -0400

    Minor format updates

--- test_cases.py
+++ test_cases.py
@@ -2,7 +2,11 @@
 ## Can be executed by running "pytest test_cases.py" in the command line
 
 import pytest
+
+# import functions for testing
 from net_pay_calculator import main, input_employee_data, get_pay_rate, calculate_gross_pay, calculate_taxes, record_results
+
+
 
 
 # Assigned to Fatimatou Ibrahim
@@ -12,7 +16,7 @@ def test_input_employee_data() -> None:
 
 # Assigned to Tyler Howard
 
-def test_get_pay_rate():
+def test_get_pay_rate() -> None:
     assert get_pay_rate(1) == 20.00
     assert get_pay_rate(2) == 35.00
     assert get_pay_rate(4) == 17.77
