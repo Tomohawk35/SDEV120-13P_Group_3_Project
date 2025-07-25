44e3d4809a89b7a6c4358199a019bbc67a403dab
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2025-07-25 15:24:00 -0400

    Updated net_pay_calculator
    Cleaned up employee data and finished up main function.

--- net_pay_calculator.py
+++ net_pay_calculator.py
@@ -3,6 +3,7 @@
 # used to manage the database file for pulling pay rates
 # more information can be found at https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html#pandas.read_csv
 import pandas as pd 
+from typing import Tuple
 
 
 # database where employee pay rates are stored
@@ -18,62 +19,68 @@ SENTINEL: str = "000"
 # while loop control
 enter_data: bool = True
 
-# employee data
-first_name: str
-last_name: str
-employee_id: int
-dependents: int
-hours_worked: float
-gross_pay: float
-net_pay: float
-
 
 # Main function is assigned to Ashley Kemp
 def main():
     # load and store the employee rates table for use in other functions
     rate_table: pd.DataFrame = connect_db()
 
+    global enter_data
+
     # create loop to continue entering data
     while enter_data == True:
 
-        # clear variables so values aren't inadvertently carried to next entry
-        first_name = None
-        last_name = None
-        employee_id = None
-        dependents = None
-        hours_worked = None
-        gross_pay = None
-        net_pay = None
+        pay_stub_data: list = []
 
         # Gather employee data
         employee_id = get_employee_id(rate_table)
-        emp_data: dict = input_employee_data(rate_table)
+        pay_stub_data.append(employee_id)
+
+        first_name, last_name, dependents, hours_worked = input_employee_data()
+        pay_stub_data.append(first_name)
+        pay_stub_data.append(last_name)
+        pay_stub_data.append(dependents)
+        pay_stub_data.append(hours_worked)
 
         # Query table for pay rate
-        emp_data['pay_rate'] = get_pay_rate(emp_data['employee_id'], rate_table)
+        pay_rate = get_pay_rate(employee_id, rate_table)
+        pay_stub_data.append(pay_rate)
 
         # Calculate gross pay
-        emp_data['gross_pay'] = calculate_gross_pay(emp_data['hours_worked'], emp_data['pay_rate'])
+        standard_hours, overtime_hours, standard_pay, overtime_pay, gross_pay = calculate_gross_pay(hours_worked, pay_rate)
+        pay_stub_data.append(standard_hours)
+        pay_stub_data.append(overtime_hours)
+        pay_stub_data.append(standard_pay)
+        pay_stub_data.append(overtime_pay)
+        pay_stub_data.append(gross_pay)
 
         # Calculate net pay
-        emp_data['net_pay'] = calculate_net_pay()
+        state_tax_ded, fed_tax_ded, net_pay = calculate_net_pay(gross_pay)
+        pay_stub_data.append(state_tax_ded)
+        pay_stub_data.append(fed_tax_ded)
+        pay_stub_data.append(net_pay)
 
-        record_results(emp_data)
+        record_results(pay_stub_data)
+        
+        value: str = do_continue()
+        while value != "y" and value != "n":
+            value = do_continue()
+        
+        if value == "n":
+            enter_data = False
+        
+    print("Program complete.")
 
-    pass
 
+def do_continue():
+    return input("Would you like to input more data? (y/n): ")
 
 # Assigned to Fatimatou Ibrahim
 def get_employee_id(emp_list: pd.DataFrame) -> int:
     # Prompt and validate employee ID
     while True:
         try:
-            value = input(f"Enter Employee ID or {SENTINEL} to exit: ")
-            if value == SENTINEL:
-                enter_data = False
-                return 000
-            
-            e_id = int(value)
+            e_id: int = int(input("Enter Employee ID: ").strip())
             if e_id not in emp_list.index:
                 print(f"Employee ID {e_id} not found in database. Try again.")
             else:
@@ -85,7 +92,7 @@ def get_employee_id(emp_list: pd.DataFrame) -> int:
 
 
 
-def input_employee_data(emp_list: pd.DataFrame) -> dict:
+def input_employee_data() -> Tuple[str, str, int, float]:
     """
     Prompts the user for employee data, validates employee_id using the database,
     and returns the collected data in a dictionary.
@@ -114,14 +121,8 @@ def input_employee_data(emp_list: pd.DataFrame) -> dict:
                 break
         except ValueError:
             print("Please enter a valid number for hours worked.")
-
-    return {
-        "first_name": first_name,
-        "last_name": last_name,
-        "employee_id": employee_id,
-        "dependents": dependents,
-        "hours_worked": hours_worked
-    }
+    
+    return first_name, last_name, dependents, hours_worked
 
 
 # Assigned to Tyler Howard
@@ -140,14 +141,14 @@ def get_pay_rate(emp_id: int, emp_rates: pd.DataFrame) -> float:
     return emp_rates['employee_rate'].loc[emp_id]
 
 
-
 # Assigned to DeMishia jackson
 def calculate_gross_pay(hours_worked: float, pay_rate: float) -> float:
-    standard_hours = min(hours_worked, 40) 
-    overtime_hours = max(hours_worked - 40, 0) 
-    standard_pay = standard_hours * pay_rate
-    overtime_pay = overtime_hours * pay_rate * 1.5
-    return round(standard_pay + overtime_pay, 2)
+    standard_hours: float = min(hours_worked, 40) 
+    overtime_hours: float = max(hours_worked - 40, 0) 
+    standard_pay: float = round(standard_hours * pay_rate, 2)
+    overtime_pay: float = round(overtime_hours * pay_rate * 1.5, 2)
+    gross_pay: float = standard_pay + overtime_pay
+    return standard_hours, overtime_hours, standard_pay, overtime_pay, gross_pay
 
 
 # Assigned to Willie Jones
@@ -155,19 +156,19 @@ def calculate_net_pay(gross_pay: float) -> float:
     state_tax_deduction = STATE_TAX * gross_pay
     federal_tax_deduction = FEDERAL_TAX * gross_pay
     net_pay = round(gross_pay - state_tax_deduction - federal_tax_deduction, 2)
-    return net_pay
+    return state_tax_deduction, federal_tax_deduction, net_pay
 
 
 # Assigned to Kevin White
 # Record FirstName, LastName, EmployeeID, NumDependents, HoursWorked, GrossPay, NetPay
-def record_results(data: dict) -> None:
-    result_data: list = [first_name, last_name, employee_id, dependents, hours_worked, gross_pay, net_pay]
-    column_headers: list = ["FirstName","LastName","EmployeeID","NumDependents","HoursWorked","GrossPay","NetPay"]
-    results_table = pd.DataFrame([result_data], columns=column_headers)
+def record_results(data: list) -> None:
+    # result_data: list = [first_name, last_name, employee_id, dependents, hours_worked, gross_pay, net_pay]
+    column_headers: list = ["EmployeeID","FirstName","LastName","NumDependents","HoursWorked","PayRate","StandardHours","OvertimeHours","StandardPay","OvertimePay","GrossPay","StateTax","FedTax","NetPay"]
+    results_table = pd.DataFrame([data], columns=column_headers)
+    print(results_table)
     results_table.to_csv("results.csv", mode="a", index=False, header=False)
 
 
 
 
-df = connect_db()
-get_employee_id(df)
+main()
